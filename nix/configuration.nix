b243fac3635050b45a7405929caada63e3dc2522
oscar370 <oscar.dga@outlook.com>
2025-11-25 16:41:20 -0600

    fix: enable Flatpak service and ensure it starts after network is online

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -106,9 +106,10 @@
   services.printing.enable = false;
   services.syncthing.enable = true;
   services.cloudflare-warp.enable = true;
-  services.flatpak.enable = true;
   systemd.services.flatpak-repo = {
     wantedBy = ["multi-user.target"];
+    after = ["network-online.target"];
+    wants = ["network-online.target"];
     path = [pkgs.flatpak];
     script = ''
       flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
