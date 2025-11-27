a60d6c4606bf6f5e45d79d577e786e87e8aa4b21
oscar370 <oscar.dga@outlook.com>
2025-11-27 09:46:43 -0600

    feat: add initial default.nix configuration with template setup

---
+++ default.nix
@@ -0,0 +1,14 @@
+{
+  description = "A simple flake with 1 template";
+
+  outputs = {self}: {
+    templates = {
+      example = {
+        path = ./default;
+        description = "Basic NixOS config flake";
+      };
+    };
+
+    defaultTemplate = self.templates.example;
+  };
+}
