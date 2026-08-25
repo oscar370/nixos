67f9b9414d78a2701b59189ed3ce070c0197beee
oscar370 <57201580+oscar370@users.noreply.github.com>
2026-08-25 08:59:35 -0600

    The Noctalia settings were added, and the Noctalia colors were imported into Niri

--- modules/home/desktop/noctalia.nix
+++ modules/home/desktop/noctalia.nix
@@ -8,8 +8,40 @@
   programs.noctalia = {
     enable = true;
     settings = {
-      osd = {
-        enable_key_osd = false;
+      bar.default = {
+        margin_ends = 0;
+        radius_top_left = 0;
+        radius_top_right = 0;
+      };
+
+      dock = {
+        enabled = true;
+        reserve_space = false;
+        smart_auto_hide = true;
+      };
+
+      osd.kinds = {
+        keyboard_backlight = false;
+        keyboard_layout = false;
+        lock_keys = false;
+        media = false;
+        privacy = false;
+      };
+
+      theme = {
+        builtin = "Kanagawa";
+        community_palette = "Oxocarbon";
+        mode = "dark";
+        source = "wallpaper";
+        wallpaper_scheme = "m3-content";
+        templates = {
+          builtin_ids = [
+            "gtk3"
+            "gtk4"
+            "niri"
+            "qt"
+          ];
+        };
       };
     };
   };
@@ -47,13 +79,5 @@
     };
   };
 
-  xdg.configFile."niri/config.kdl".source =
-    pkgs.runCommand "niri-config-checked"
-      {
-        nativeBuildInputs = [ pkgs.niri ];
-      }
-      ''
-        niri validate --config ${../../utils/niri/config.kdl}
-        cp ${../../utils/niri/config.kdl} $out
-      '';
+  xdg.configFile."niri/config.kdl".source = ../../utils/niri/config.kdl;
 }
